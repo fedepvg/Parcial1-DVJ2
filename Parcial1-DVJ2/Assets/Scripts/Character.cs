75f39e89e7c76f095c4424b44fa41a07a8c445e5
fedepvg <federicopvg@gmail.com>
2019-05-18 13:10:07 -0300

    movement improved

--- Parcial1-DVJ2/Assets/Scripts/Character.cs
+++ Parcial1-DVJ2/Assets/Scripts/Character.cs
@@ -13,41 +13,46 @@ public class Character : MonoBehaviour
     Vector2 Movement;
     Directions MoveDir;
     Directions TurnDirection=Directions.none;
+    const float CenterPosition = 0.5f;
+    const float UpperLimitForTurn = 0.55f;
+    const float LowerLimitForTurn = 0.45f;
+    bool[] PosibleMovement = new bool[(int)Directions.none];
+    public LayerMask RaycastLayer;
+    const float RayDistance = 0.51f;
 
     private void Start()
     {
         MoveDir = Directions.forward;
+        for(int i=0;i<(int)Directions.none; i++)
+        {
+            PosibleMovement[i] = true;
+        }
     }
 
     void Update()
     {
         if (Input.anyKey)
         {
-            if (Mathf.FloorToInt(transform.position.x) % 2 == 0)
+            OnLimitWall();
+            if (Input.GetKey(KeyCode.UpArrow) && PosibleMovement[(int)Directions.forward])
             {
-                if (Input.GetKey(KeyCode.UpArrow))
-                {
-                    transform.position += Vector3.forward * Speed * Time.deltaTime;
-                    MoveDir = Directions.forward;
-                }
-                else if (Input.GetKey(KeyCode.DownArrow))
-                {
-                    transform.position += Vector3.back * Speed * Time.deltaTime;
-                    MoveDir = Directions.back;
-                }
+                transform.position += Vector3.forward * Speed * Time.deltaTime;
+                MoveDir = Directions.forward;
             }
-            if (Mathf.FloorToInt(transform.position.z) % 2 == 0)
+            else if (Input.GetKey(KeyCode.DownArrow) && PosibleMovement[(int)Directions.back])
             {
-                if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    transform.position += Vector3.left * Speed * Time.deltaTime;
-                    MoveDir = Directions.left;
-                }
-                else if (Input.GetKey(KeyCode.RightArrow))
-                {
-                    transform.position += Vector3.right * Speed * Time.deltaTime;
-                    MoveDir = Directions.right;
-                }
+                transform.position += Vector3.back * Speed * Time.deltaTime;
+                MoveDir = Directions.back;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) && PosibleMovement[(int)Directions.left])
+            {
+                transform.position += Vector3.left * Speed * Time.deltaTime;
+                MoveDir = Directions.left;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) && PosibleMovement[(int)Directions.right])
+            {
+                transform.position += Vector3.right * Speed * Time.deltaTime;
+                MoveDir = Directions.right;
             }
             CheckSnapping();
         }
@@ -82,12 +87,12 @@ public class Character : MonoBehaviour
 
     void SetX()
     {
-        transform.position = new Vector3(Mathf.CeilToInt(transform.position.x) - 0.5f, transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.CeilToInt(transform.position.x) - CenterPosition, transform.position.y, transform.position.z);
     }
 
     void SetZ()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.CeilToInt(transform.position.z) - 0.5f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.CeilToInt(transform.position.z) - CenterPosition);
     }
 
     bool CheckOnCorner()
@@ -98,12 +103,12 @@ public class Character : MonoBehaviour
         {
             case Directions.back:
             case Directions.forward:
-                if (roundedX > 0.65f)
+                if (roundedX > UpperLimitForTurn)
                 {
                     TurnDirection = Directions.left;
                     return true;
                 }
-                else if (roundedX < 0.35f)
+                else if (roundedX < LowerLimitForTurn)
                 {
                     TurnDirection = Directions.right;
                     return true;
@@ -115,12 +120,12 @@ public class Character : MonoBehaviour
                 }
             case Directions.right:
             case Directions.left:
-                if (roundedZ > 0.65f)
+                if (roundedZ > UpperLimitForTurn)
                 {
                     TurnDirection = Directions.back;
                     return true;
                 }
-                else if (roundedZ < 0.35f)
+                else if (roundedZ < LowerLimitForTurn)
                 {
                     TurnDirection = Directions.forward;
                     return true;
@@ -151,6 +156,68 @@ public class Character : MonoBehaviour
             case Directions.right:
                 transform.position += Vector3.right * Speed * Time.deltaTime;
                 break;
+        }
+    }
+
+    void OnLimitWall()
+    {
+        RaycastHit hit;
+        string layerHitted;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, RayDistance, RaycastLayer))
+        {
+            layerHitted = LayerMask.LayerToName(hit.transform.gameObject.layer);
+
+            if (layerHitted == "Wall")
+            {
+                PosibleMovement[(int)Directions.forward] = false;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.forward * RayDistance, Color.yellow);
+            PosibleMovement[(int)Directions.forward] = true;
+        }
+        if (Physics.Raycast(transform.position, Vector3.back, out hit, RayDistance, RaycastLayer))
+        {
+            layerHitted = LayerMask.LayerToName(hit.transform.gameObject.layer);
+
+            if (layerHitted == "Wall")
+            {
+                PosibleMovement[(int)Directions.back] = false;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.back * RayDistance, Color.yellow);
+            PosibleMovement[(int)Directions.back] = true;
+        }
+        if (Physics.Raycast(transform.position, Vector3.left, out hit, RayDistance, RaycastLayer))
+        {
+            layerHitted = LayerMask.LayerToName(hit.transform.gameObject.layer);
+
+            if (layerHitted == "Wall")
+            {
+                PosibleMovement[(int)Directions.left] = false;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.left * RayDistance, Color.yellow);
+            PosibleMovement[(int)Directions.left] = true;
+        }
+        if (Physics.Raycast(transform.position, Vector3.right, out hit, RayDistance, RaycastLayer))
+        {
+            layerHitted = LayerMask.LayerToName(hit.transform.gameObject.layer);
+
+            if (layerHitted == "Wall")
+            {
+                PosibleMovement[(int)Directions.right] = false;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.right * RayDistance, Color.yellow);
+            PosibleMovement[(int)Directions.right] = true;
         }
     }
 }
